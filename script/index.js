3f235f46ba53deac37acfce0c38d222039ccbbaf
Vlados <vlad8aromanov@gmail.com>
2020-11-09 15:55:13 +0200

    commit 5

--- script/index.js
+++ script/index.js
@@ -1,5 +1,3 @@
 const $like = document.querySelector("#like");
 $like.addEventListener("click", () => ($like.style.color = "red"));
 
-const $text = document.querySelector("#yeartext");
-$text.innerHTML += new Date().getFullYear();
